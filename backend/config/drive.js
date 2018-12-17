6217c69a68c225825cd98594a48f4cf1a7dea703
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-17 22:48:12 +0100

    event email verification-- send un email to user add,page for email  verificatin add

--- backend/config/drive.js
+++ backend/config/drive.js
@@ -4,7 +4,7 @@ const Helpers = use('Helpers')
 const Env = use('Env')
 
 module.exports = {
-  /*
+	/*
   |--------------------------------------------------------------------------
   | Default disk
   |--------------------------------------------------------------------------
@@ -13,10 +13,10 @@ module.exports = {
   | defining a disk name
   |
   */
-  default: 'local',
+	default: 'local',
 
-  disks: {
-    /*
+	disks: {
+		/*
     |--------------------------------------------------------------------------
     | Local
     |--------------------------------------------------------------------------
@@ -24,12 +24,12 @@ module.exports = {
     | Local disk interacts with the a local folder inside your application
     |
     */
-    local: {
-      root: Helpers.tmpPath(),
-      driver: 'local'
-    },
+		local: {
+			root: Helpers.tmpPath(),
+			driver: 'local'
+		},
 
-    /*
+		/*
     |--------------------------------------------------------------------------
     | S3
     |--------------------------------------------------------------------------
@@ -37,12 +37,12 @@ module.exports = {
     | S3 disk interacts with a bucket on aws s3
     |
     */
-    s3: {
-      driver: 's3',
-      key: Env.get('S3_KEY'),
-      secret: Env.get('S3_SECRET'),
-      bucket: Env.get('S3_BUCKET'),
-      region: Env.get('S3_REGION')
-    }
-  }
+		s3: {
+			driver: 's3',
+			key: Env.get('S3_KEY'),
+			secret: Env.get('S3_SECRET'),
+			bucket: Env.get('S3_BUCKET'),
+			region: Env.get('S3_REGION')
+		}
+	}
 }
--- backend/test/unit/user.spec.js
+++ backend/test/unit/user.spec.js
@@ -4,21 +4,21 @@ const { test, after } = use('Test/Suite')('Model user')
 const User = use('App/Models/User')
 
 after(async () => {
-    await User.query().delete()
+	await User.query().delete()
 })
 test('Save user in database', async ({ assert }) => {
-    let user = new User({name:'Jairo',location:[121221,232323],email:'test@gmail.com'});
-    await user.save()
-    let user_ = await User.find(user._id)
-    assert.equal(user.name,user_.name)
+	let user = new User({name:'Jairo',location:[121221,232323],email:'test@gmail.com'})
+	await user.save()
+	let user_ = await User.find(user._id)
+	assert.equal(user.name,user_.name)
 })
 
 test('get user by name in database', async ({ assert }) => {
-    let user
-    try {
-        user = await User.findBy('name', 'Jairo')
-    } catch (e) {
-        console.error(e);
-    }
-    assert.equal(user.email,'test@gmail.com')
+	let user
+	try {
+		user = await User.findBy('name', 'Jairo')
+	} catch (e) {
+		console.error(e)
+	}
+	assert.equal(user.email,'test@gmail.com')
 })
