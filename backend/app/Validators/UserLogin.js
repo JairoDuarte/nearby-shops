3c07b82569a33b3ab745f3772c1aba8cf26dd9d1
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-17 21:09:28 +0100

    route user add,filds validation for register and sign in add

---
+++ backend/app/Validators/UserLogin.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const BaseValidator = require('./BaseValidator')
+
+class User extends BaseValidator {
+  get rules () {
+    return {
+      email: 'required|email',
+      password: 'required|min:8|max:256'
+    }
+  }
+}
+
+module.exports = User
