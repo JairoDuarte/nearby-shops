059dd8ca2ce74268f59a73b88944d57b75e98d7e
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-18 04:04:47 +0100

    athentification add(singnup, signin,signgout,etc )

---
+++ backend/app/Controllers/Http/UserController.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const User = use('App/Models/User')
+const crypto = use('crypto')
+const uuid = use('uuid')
+const Event = use('Event')
+
+class UserController {
+
+}
+
+module.exports = UserController
