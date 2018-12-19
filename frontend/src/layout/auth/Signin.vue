c894e949e05e8eb64138d85dd30d2dd5f45c03cb
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-19 14:35:27 +0100

    component signup and sign in add

---
+++ frontend/src/layout/auth/Signin.vue
@@ -0,0 +1,146 @@
+<template>
+    <section class="section section-shaped section-lg my-0">
+    
+        <div class="container pt-lg-md">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <card type="secondary" shadow
+                          header-classes="bg-white pb-5"
+                          body-classes="px-lg-5 py-lg-5"
+                          class="border-0">
+                        <template>
+                            <div class="text-muted text-center mb-3">
+                                <small>Sign in with</small>
+                            </div>
+                            <div class="btn-wrapper text-center">
+                                <base-button type="neutral">
+                                    <img slot="icon" src="img/icons/common/google.svg">
+                                    Google
+                                </base-button>
+                            </div>
+                        </template>
+                        <template>
+                            <div class="text-center text-muted mb-4">
+                                <small>Or sign in with credentials</small>
+                            </div>
+                            <form role="form">
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Email"
+                                            addon-left-icon="ni ni-email-83">
+                                </base-input>
+                                <base-input alternative
+                                            type="password"
+                                            placeholder="Password"
+                                            addon-left-icon="ni ni-lock-circle-open">
+                                </base-input>
+                               
+                                <div class="text-center">
+                                    <base-button type="primary" class="my-4">Sign In</base-button>
+                                </div>
+                            </form>
+                        </template>
+                    </card>
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <a href="#" class="text-light">
+                                <small></small>
+                            </a>
+                        </div>
+                        <div class="col-6 text-right">
+                            <a href="#" @click="modalshow" class="text-light">
+                                <small >Create new account</small>
+                            </a>
+                            <modal :show.sync="active"
+                   body-classes="p-0"
+                   modal-classes="modal-dialog-centered modal-sm">
+                   <card type="secondary" shadow
+                          header-classes="bg-white pb-5"
+                          body-classes="px-lg-5 py-lg-5"
+                          class="border-0">
+                        <template>
+                            <div class="text-muted text-center mb-3">
+                                <small>Sign in with</small>
+                            </div>
+                            <div class="btn-wrapper text-center">
+                                <base-button type="neutral">
+                                    <img slot="icon" src="img/icons/common/google.svg">
+                                    Google
+                                </base-button>
+                            </div>
+                        </template>
+                        <template>
+                            <div class="text-center text-muted mb-4">
+                                <small>Or sign up with credentials</small>
+                            </div>
+                            <form role="form">
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Name"
+                                            addon-left-icon="ni ni-hat-3">
+                                </base-input>
+                                <base-input alternative
+                                            class="mb-3"
+                                            placeholder="Email"
+                                            addon-left-icon="ni ni-email-83">
+                                </base-input>
+                                <base-input alternative
+                                            type="password"
+                                            placeholder="Password"
+                                            addon-left-icon="ni ni-lock-circle-open">
+                                </base-input>
+                                <div class="text-muted font-italic">
+                                    <small>password strength:
+                                        <span class="text-success font-weight-700">strong</span>
+                                    </small>
+                                </div>
+                                <base-checkbox>
+                                    <span>I agree with the
+                                        <a href="#">Privacy Policy</a>
+                                    </span>
+                                </base-checkbox>
+                                <div class="text-center">
+                                    <base-button type="primary" class="my-4">Sign up</base-button>
+                                </div>
+                            </form>
+                        </template>
+                    </card>
+
+                            </modal>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+import Modal from "@/components/Modal.vue";
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      active: false
+    };
+  },
+  methods: {
+    modalshow() {
+      this.active = !this.active;
+    }
+  }
+};
+</script>
+<style>
+.section {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+#main {
+  height: 100vh;
+}
+</style>
