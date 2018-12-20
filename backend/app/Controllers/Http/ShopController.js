1c9aa419e909a42b738f691df309852d64d946a1
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-20 20:05:35 +0100

    .

---
+++ backend/app/Controllers/Http/ShopController.js
@@ -0,0 +1,36 @@
+'use strict'
+const Env = use('Env')
+const Shop = use('App/Models/Shop')
+
+const googleMaps = require('@google/maps').createClient({
+	key: Env.get('GOOGLE_MAP_KEY'),
+	Promise: Promise
+})
+
+class ShopController {
+
+    	/**
+     * 
+     * User register
+     * request in google map shops by user location and 
+     * return all shops ranked by distance
+     * ['supermarket','shoe_store','shopping_mall','store']
+     */
+	async getAll ({ request, response,params }) {
+		try {
+			const shops = await googleMaps.placesNearby({
+				location: [params.lat, params.long],
+				rankby: 'distance',
+				type: 'store'
+			}).asPromise()
+			return response.status(200).json({ shops:shops.json.results})
+
+		} catch (error) {
+      return response.status(404).json({ message: 'Shops not found ',error})
+		}
+  }
+
+
+}
+
+module.exports = ShopController
