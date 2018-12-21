836963704af03cde0c3e1c8026fcd9d7c73b57b9
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-21 19:29:37 +0100

    User like dislike and remve shop feature add

--- backend/app/Controllers/Http/ShopController.js
+++ backend/app/Controllers/Http/ShopController.js
@@ -1,6 +1,5 @@
 'use strict'
 const Env = use('Env')
-const Shop = use('App/Models/Shop')
 
 const googleMaps = require('@google/maps').createClient({
 	key: Env.get('GOOGLE_MAP_KEY'),
@@ -16,7 +15,7 @@ class ShopController {
      * return all shops ranked by distance
      * ['supermarket','shoe_store','shopping_mall','store']
      */
-	async getAll ({ request, response,params }) {
+	async getAll ({ response,params }) {
 		try {
 			const shops = await googleMaps.placesNearby({
 				location: [params.lat, params.long],
@@ -26,9 +25,9 @@ class ShopController {
 			return response.status(200).json({ shops:shops.json.results})
 
 		} catch (error) {
-      return response.status(404).json({ message: 'Shops not found ',error})
+			return response.status(404).json({ message: 'Shops not found ',error})
 		}
-  }
+	}
 
 
 }
