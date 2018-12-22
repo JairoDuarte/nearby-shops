e6b0acc600e62810f0a8e2a565d679a5b01814c6
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-22 16:38:58 +0100

    .

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
+    "no-unused-vars": "off",
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
